17a0defd9766cb12419b2e97fe74d9b6d8a39337
Aleksandr Sinotov <bratsinot@gmail.com>
2023-02-10 12:13:34 +0100

    `timeout` and `interval` sets through `core::time::Duration`.

--- src/consts.rs
+++ src/consts.rs
@@ -1,7 +1,7 @@
 use core::time::Duration;
 
-pub(crate) const DEFAULT_TIMEOUT_MS: u64 = 60000;
-pub(crate) const DEFAULT_INTERVAL_MS: u64 = 0;
+pub(crate) const DEFAULT_TIMEOUT_MS: Duration = Duration::from_secs(60);
+pub(crate) const DEFAULT_INTERVAL_MS: Duration = Duration::new(0, 0);
 pub(crate) const DEFAULT_EVENT_CHANNEL_CAPACITY: usize = 500;
 pub(crate) const DEFAULT_COMMAND_CHANNEL_CAPACITY: usize = 2;
 pub(crate) const DEFAULT_NUMBER_OF_REHANDSHAKE: usize = 3;
